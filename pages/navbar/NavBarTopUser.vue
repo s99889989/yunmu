30d097c05d249a5556c9b4e6177ecb83857776f9
BuildTools <unconfigured@null.spigotmc.org>
2023-11-17 22:12:41 +0800

    2023/11/17

--- pages/navbar/NavBarTopUser.vue
+++ pages/navbar/NavBarTopUser.vue
@@ -33,6 +33,11 @@ const dark_mode = useDarkModeStore();
           </li>
 
           <li>
+            <a href="https://discord.gg/KUeh2zeTU7 " target="_blank"  class="text-2xl block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+              車隊群</a>
+          </li>
+
+          <li>
             <NuxtLink to="/account/login" class="text-2xl block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
               帳戶</NuxtLink>
           </li>
